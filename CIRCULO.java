00233605fab73338600fdf91c477b190bda8ae68
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-01-23 09:46:48 -0600

    Update CIRCULO.java

--- CIRCULO.java
+++ CIRCULO.java
@@ -2,21 +2,23 @@ package package1;
 
 public class CIRCULO {
 	
-	public double radiox;
+	public double radio;
 	public double circunferencia, area, pi;
 	public CIRCULO(float radio) {
-		radiox = radio;
+		this.radio = radio;
 		//c=2pi*R
 		//A=pi*r^2
 		pi=Math.PI;
-		circunferencia = (2*pi)*radiox;
-		area=pi*((double)Math.pow(radiox, 2));
 	}
-	
+	CIRCULO(){
+		this(178607);
+	}
 	public double impresionA() {
+		circunferencia = (2*pi)*radio;
 		return circunferencia;
 	}
 	public double impresionB() {
+		area=pi*((double)Math.pow(radio, 2));
 		return area;
 	}
 }
